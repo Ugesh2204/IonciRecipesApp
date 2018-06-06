eb7be4a22305ea4cafb9862a2716befbc7fc6458
ugesh <ugesh.roopchand@hotmail.com>
2018-06-06 09:24:05 +0400

    Service, model, add and remove done

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 import { ShoppingListService } from '../../services/shopping-list-service';
+import { Ingredient } from '../../models/ingredient';
 
 
 
@@ -12,13 +13,35 @@ import { ShoppingListService } from '../../services/shopping-list-service';
 })
 export class ShoppingListPage {
 
+  /**Property list display list */
+  listItems: Ingredient[];
+
   /**Make the service avaible in this component */
-  constructor(private slService: ShoppingListService) {}
+  constructor(private slService: ShoppingListService) { }
+
+  /** getItems() from service will get a copy of the item*/
+  ionViewWillEnter() {
+    this.loadItems();
+  }
 
   /**form you will recive  */
   /**Use the methdo from addItem which is found in service */
   onAddItem(form: NgForm) {
     this.slService.addItem(form.value.ingredientName, form.value.amount);
     form.reset();
+    /**This will display to the page itselft and get lastest version*/
+    this.loadItems();
   }
+
+  onRemoveItem(index: number) {
+    this.slService.removeItem(index);
+    /**Need to reload it here */
+    this.loadItems();
+  }
+
+  /**Helper metho get the copy */
+  private loadItems() {
+    this.listItems = this.slService.getItems();
+  }
+
 }
